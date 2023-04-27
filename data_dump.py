0fb74792bc079f22690f4649928e633f3e543901
Rakeshakhil <rakei9820@gmail.com>
2023-04-27 22:30:30 +0530

    utils file config file

--- data_dump.py
+++ data_dump.py
@@ -4,18 +4,18 @@ import pymongo
 import json
 
 from pymongo.mongo_client import MongoClient
-
-uri = "mongodb+srv://Rakesh:Rakesh26@cluster0.vj4c6b7.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client =pymongo.MongoClient("mongodb+srv://Rakesh:Rakesh26@cluster0.vj4c6b7.mongodb.net/?retryWrites=true&w=majority")
 
 # Create a new client and connect to the server
-client = MongoClient(uri)
+
 
 # Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+#try:
+    #client.admin.command('ping')
+    #print("Pinged your deployment. You successfully connected to MongoDB!")
+#except Exception as e:
+    #print(e)
 #Creating a database in MongoDB
 Data_file_path=(r'C:\Users\rakei\OneDrive\Desktop\End to End ML Project\End-to-End-ML-Project\insurance.csv')
 Database_name='Insurance'
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
-import os,sys
+import os
+import sys
+from Insurance.utils import get_collection_as_dataframe
 
-def test_logger_and_exception():
-    
+
+#def test_logger_and_exception():
+
+    # try:
+    #logging.info('Starting test logger and exception')
+    # result=3/0
+    # print(result)
+    #logging.info('Finished test logger and exception')
+    # except Exception as e:
+    # logging.debug(str(e))
+    #raise InsuranceException(e,sys)
+
+
+if __name__ == '__main__':
     try:
-        logging.info('Starting test logger and exception')
-        result=3/0
-        print(result)
-        logging.info('Finished test logger and exception')
+        # start_training_pipelining()
+        get_collection_as_dataframe(database_name='Insurance', collection_name='Insurance_Project')
     except Exception as e:
-        logging.debug(str(e))
-        raise InsuranceException(e,sys)
-    
-if __name__=='__main__':
-    try:
-        test_logger_and_exception()
-    except Exception as e:
-        print(e)
-        
+        print('e')
